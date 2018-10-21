a44454e46e0d2e6ce8649a68a780392172df7530
NickZhao <nickzrz@hotmail.com>
2018-10-21 12:28:57 -0700

    Need to finished AddVertex

---
+++ All_Graphs/Graph.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All_Graphs
+{
+    class Graph<T> where T : IComparable
+    {
+
+        List<Vertex<T>> Vertices = new List<Vertex<T>>();
+        List<Edge<T>> Edges = new List<Edge<T>>();
+
+        public Graph()
+        {
+        }
+        void DepthFirstTraversal(Vertex<T> start)
+        {
+            throw new NotImplementedException();
+        }
+        void BreadthFirstTraversal(Vertex<T> start)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ All_Graphs/Vertex.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All_Graphs
+{
+    class Vertex<T> where T : IComparable
+    {
+        T value;
+
+        public Vertex()
+        {
+        }
+        void AddVertex(Vertex<T> v)
+        {
+
+            throw new NotImplementedException();
+        }
+        void RemoveVertex(Vertex<T> v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
